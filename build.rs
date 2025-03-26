d4676af7b0779adb78e5836ac95845789cbe37c0
KaidenSmith <KdntNinja-2010@proton.me>
2025-03-26 20:11:30 +0000

    Remove unused build script for diesel migrations

--- build.rs
+++
@@ -1,7 +0,0 @@
-extern crate diesel_migrations;
-
-fn main() {
-    // This build script is used to ensure that migrations are properly
-    // processed at compile time
-    println!("cargo:rerun-if-changed=migrations");
-}
